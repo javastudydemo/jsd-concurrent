ef68590412d5e37dc2040cce436f58813e7cc38a
ijiangtao <jiangtao.li@baozun.com>
2019-03-22 12:00:53 +0800

    init

---
+++ jsd-concurrent/src/main/java/net/ijiangtao/tech/concurrent/jsd/JsdConcurrentApplication.java
@@ -0,0 +1,13 @@
+package net.ijiangtao.tech.concurrent.jsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsdConcurrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsdConcurrentApplication.class, args);
+	}
+
+}
